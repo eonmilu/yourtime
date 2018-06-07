c9fa1bb151ed024b9ade64df3b9f3cb665c17615
eonmilu <eonmilu@gmail.com>
2018-06-08 01:17:41 +0200

    Dynamic redirect fix, refactor

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 const SELECT_URL = "https://oxygenrain.com/yourtime/search.php?";
 const INSERT_URL = "https://oxygenrain.com/yourtime/insert.php?";
 const STYLESHEET_URL = document.getElementsByTagName('meta')['stylesheet-internal-url'].getAttribute('content');
-var isLoaded = false;
-// youtube's dynamic redirect refers to the tech yt uses to load the page when clicking a timemark on a comment or another video
+var lastId = "";
 
 function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
@@ -10,205 +9,166 @@ function httpGetAsync(theUrl, callback) {
         function () {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
                 callback(xmlHttp.responseText);
-        }
+        };
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
     xmlHttp.send(null);
 }
 
-// transforms seconds to (days):(hours):minutes:seconds
-function SecondsToString(ss) {
-    if (ss < 0) {
-        return "negative time";
-    }
+// Transform seconds to (days):(hours):minutes:seconds
+function secondsToString(ss) {
+    // Ignore negative seconds
+    if (ss < 0) return Global.NaN;
+
     var dd = Math.floor(ss / (3600 * 24));
     ss -= dd * 3600 * 24;
     var hh = Math.floor(ss / 3600);
     ss -= hh * 3600;
     var mm = Math.floor(ss / 60);
     ss -= mm * 60;
-    // These lines ensure you have two-digits
-    if (dd < 10) {
+
+    // Ensure two-digits
+    if (dd < 10)
         dd = "0" + dd;
-    }
-    if (hh < 10) {
+    if (hh < 10)
         hh = "0" + hh;
-    }
-    if (mm < 10) {
+    if (mm < 10)
         mm = "0" + mm;
-    }
-    if (ss < 10) {
+    if (ss < 10)
         ss = "0" + ss;
-    }
+
     var time = dd + ":" + hh + ":" + mm + ":" + ss;
 
-    // removes unnecessary "00:"s, ignoring the last two for formatting
+    // Remove unnecessary "00:"s, ignoring the last two for formatting
     return time.replace(/^(00\:){1,2}/gm, "");
 }
 
-// turns 1000 to 1k etc
-// !TODO support >1 million
+// Make numbers human-readable
 function readablizeNumber(number) {
+    // TODO:: support >1 million
     var s = ['', 'k', 'M'];
     var e = Math.floor(Math.log(number) / Math.log(1000));
     return (number / Math.pow(1000, e)) + s[e];
 }
 
-// parses and adds the response to the dom
-function ProccessResponse(rp) {
-    var statusCode = rp.split("|")[0];
+// Add main skeleton, where the children will be added
+function addMainStructure() {
+    var stylesheet = document.createElement("link");
+    stylesheet.rel = "stylesheet", stylesheet.href = STYLESHEET_URL;
+
+    var mainStructure = document.createElement("div");
+    mainStructure.id = "your-time";
+
+    mainStructure.appendChild(stylesheet);
+    document.getElementById("info-contents").appendChild(mainStructure);
+}
+
+function appendChildToMainStructure(mainStructure, childData) {
+    var submission = document.createElement("div");
+    var votes = document.createElement("div");
+    var upvote = document.createElement("img");
+    var number = document.createElement("span");
+    var downvote = document.createElement("img");
+    var seconds = document.createElement("a");
+    var comment = document.createElement("span");
+
+    submission.className = "submission";
+    votes.className = "votes";
+    upvote.className = "upvote";
+    number.className = "number";
+    downvote.className = "downvote";
+    seconds.className = "seconds";
+    comment.className = "comment";
+
+    upvote.src = browser.extension.getURL("img/arrow_default_up-48x48.png");
+    downvote.src = browser.extension.getURL("img/arrow_default_down-48x48.png");
+
+    number.innerText = readablizeNumber(element["votes"]);
+    seconds.innerText = secondsToString(parseInt(element["time"]));
+    seconds.href = "/watch?v=" + element["video"] + "&t=" + element["time"] + "s";
+    seconds.rel = "nofollow";
+    comment.innerText = element["comment"];
+
+    votes.appendChild(upvote);
+    votes.appendChild(number);
+    votes.appendChild(downvote);
+
+    submission.appendChild(votes);
+    submission.appendChild(seconds);
+    submission.appendChild(comment);
+
+    mainStructure.appendChild(submission);
+}
+
+// Parse and add the response to the DOM
+function processResponse(rp, statusCode) {
+    var mainStructure = document.getElementById("your-time");
+
     switch (statusCode) {
+        case "220": // 220 for data returned
+            var response = JSON.parse(rp);
+            response.forEach(element => {
+                appendChildToMainStructure(mainStructure, element);
+            });
+            break;
         case "404":
-            NotFound();
-            break;
-        case "220": // response code 220 means the server has the data and was returned
-            var response = JSON.parse(rp.substr(4));
-            ResponseToDOM(response);
-            break;
-        case "210": //response code 210 means a seeder has the data
-            // !TODO add webrtc support to enable p2p
-            var response = JSON.parse(rp.substr(4));
-            break;
         default:
-            console.log("Invalid code status returned from server. Are you using the latest Your Time version?");
-            break;
+            var error = document.createElement("div"),
+                mainText = document.createElement("span"),
+                secondaryText = document.createElement("a");
+
+            error.id = "error";
+            mainText.className = "main-text";
+            mainText.innerText = "Your Time didn't find any timemarks for this video. ";
+            secondaryText.className = "secondary-text";
+            secondaryText.onclick = addTimemark;
+            secondaryText.innerText = "Submit your own.";
+
+            error.appendChild(mainText);
+            error.appendChild(secondaryText);
+            mainStructure.appendChild(error);
+
+            document.getElementById("info-contents").appendChild(mainStructure);
     }
 }
 
-function ResponseToDOM(array) {
-    var stylesheet = document.createElement("link");
-    stylesheet.rel = "stylesheet";
-    stylesheet.href = STYLESHEET_URL;
-
-    var child = document.createElement("div");
-    child.id = "your-time";
-    document.getElementById("info").appendChild(child);
-    // add stylesheet to the your-time div since i cant seem to add to head !TODO
-    document.getElementById("your-time").appendChild(stylesheet);
-
-    array.forEach(element => {
-        // great dom manipulation
-        var submission = document.createElement("div");
-        var votes = document.createElement("div");
-        var upvote = document.createElement("img");
-        var number = document.createElement("span");
-        var downvote = document.createElement("img");
-        var seconds = document.createElement("a");
-        var comment = document.createElement("span");
-
-        submission.className = "submission";
-        votes.className = "votes";
-        upvote.className = "upvote";
-        number.className = "number";
-        downvote.className = "downvote";
-        seconds.className = "seconds";
-        comment.className = "comment";
-
-        upvote.src = browser.extension.getURL("img/arrow_default_up-48x48.png");
-        downvote.src = browser.extension.getURL("img/arrow_default_down-48x48.png");
-
-        number.innerText = readablizeNumber(element["votes"]);
-        seconds.innerText = SecondsToString(parseInt(element["time"]));
-        seconds.href = "/watch?v=" + element["video"] + "&t=" + element["time"] + "s";
-        seconds.rel = "nofollow";
-        comment.innerText = element["comment"];
-
-        votes.appendChild(upvote);
-        votes.appendChild(number);
-        votes.appendChild(downvote);
-
-        submission.appendChild(votes);
-        submission.appendChild(seconds);
-        submission.appendChild(comment);
-
-        child.appendChild(submission);
-    });
-}
-
-function NotFound() {
-    var stylesheet = document.createElement("link");
-    stylesheet.rel = "stylesheet";
-    // TODO: add a meta tag containing the stylesheets' url
-    stylesheet.href = STYLESHEET_URL;
-
-    var child = document.createElement("div");
-    child.id = "your-time";
-    child.appendChild(stylesheet);
-
-    var error = document.createElement("div");
-    var mainText = document.createElement("span");
-    var secondaryText = document.createElement("a");
-
-    error.id = "error";
-    mainText.className = "main-text";
-    secondaryText.className = "secondary-text";
-
-    secondaryText.onclick = AddTimemarkCreation;
-
-    mainText.innerText = "Your Time didn't find any timemarks for this video.";
-    secondaryText.innerText = "Submit your own.";
-
-    error.appendChild(mainText);
-    error.appendChild(secondaryText);
-
-    child.appendChild(error);
-
-    var intervalId;
-    intervalId = setInterval(function () {
-        // false if it has not loaded.
-        if (document.getElementById("info-contents")) {
-            console.log("Page loaded.");
-            document.getElementById("info-contents").appendChild(child);
-            console.log("Child appended.");
-            clearInterval(intervalId);
-        }
-    }, 100);
-}
-
-// Overriding YouTube's saveAndPush function to add a event firer
-var originalSaveAndPush = saveAndPush();
-
-saveAndPush = function(arguments) {
-    console.log("YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-    return originalSaveAndPush(arguments);
-}
-
-var player = document.getElementById("movie_player");
-player.addEventListener("onStateChange", function (statusInteger) {
-    // https://developers.google.com/youtube/iframe_api_reference#Events
-    console.log(statusInteger);
-    switch (statusInteger) {
-        case 1:
-            if (!isLoaded) {
-                console.log(statusInteger + " LOADING");
-                RemoveOnError();
-                main();
-                isLoaded = true;
-                console.log("LOADED");
-            }
-            break;
-        case 5:
-            RemoveOnError();
-            main();
-            isLoaded = true;
-            console.log("LOADED");
-            break;
-    }
-});
-
-function RemoveOnError() {
+function removeMainStructure() {
     while (document.getElementById("your-time")) {
         document.getElementById("info-contents").removeChild(document.getElementById("your-time"));
     }
 }
 
-function AddTimemarkCreation() {
+function addTimemark() {
     alert("TODO:");
 }
 
+// Get ID of YouTube video from URL
+var id = window.location.href.match(/v=[^&]*/)[0];
+lastId = id;
+
+var player = document.getElementById("movie_player");
+player.addEventListener("onStateChange", function (statusInteger) {
+    // https://developers.google.com/youtube/iframe_api_reference#Events
+    // Normally we would use -1 or 5 but YouTube's API is unreliable
+    // Therefore we will use 1 (playing) and check if the data has already been loaded
+    console.log(statusInteger);
+    id = window.location.href.match(/v=[^&]*/)[0];
+    switch (statusInteger) {
+        case 1:
+            if (lastId != id) {
+                lastId = id;
+                removeMainStructure();
+                main();
+            }
+            break;
+    }
+});
+
 function main() {
-    // gets id of youtube video from url
-    var id = window.location.href.match(/v=[^&]*/);
     httpGetAsync(SELECT_URL + id, function (rp) {
-        ProccessResponse(rp);
+        var statusCode = rp.split("|")[0];
+        var response = rp.substr(4);
+
+        addMainStructure();
+        processResponse(response, statusCode);
     });
 }
