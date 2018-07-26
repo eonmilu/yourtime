4a1f272caff361a465747c652b8c4dc9e8ec720e
eonmilu <eonmilu@gmail.com>
2018-07-26 14:07:01 +0200

    TypeScript/ jQuery rewrite

---
+++ src/firefox/scripts/scriptloader.ts
@@ -0,0 +1,38 @@
+declare var browser: any;
+// This meta element contains the stylesheet, upvote and downvote images' internal url
+// so that the injected script can access them since it cannot use the browser's APIs
+var metaSources = {
+    stylesheet: browser.extension.getURL("../resources/stylesheet.css"),
+    loader: browser.extension.getURL("../resources/loader.svg")
+}
+
+let jquery = $("<script/>", {src: browser.extension.getURL("scripts/jquery.min.js")});
+let script = $("<script/>", {src: browser.extension.getURL("scripts/content.js")});
+
+let loader = $("<img/>", {
+    src: browser.extension.getURL("../resources/loader.svg"),
+    alt: "Loading...",
+    id: "your-time-loader",
+    height: "45px", width: "65px",
+    style: "display: block; margin: auto;"
+});
+
+let meta = $("<meta/>", {
+    name: "your-time-meta",
+    content: JSON.stringify(metaSources)
+});
+
+// Check every 10 ms if the div has been loaded
+var intervalId;
+intervalId = setInterval(() => {
+    console.log("Searching for target div...");
+
+    // false if it has not loaded.
+    if ($("#info-contents").length) {
+        console.log("Found target div. Adding script...");
+
+        $("#info-contents").append(loader);
+        $("head").append(jquery, meta, script);
+        clearInterval(intervalId);
+    }
+}, 10);
