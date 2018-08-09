2c4b72d14d3ff55ebef7fd11e57543c627cd8a0a
eonmilu <eonmilu@gmail.com>
2018-08-10 01:03:59 +0200

    Popup files

--- src/firefox/scripts/popup.ts
+++ src/firefox/scripts/popup.ts
@@ -1,1 +1,29 @@
-$("#text").text("Hello there!");
+// Enable or disable the extension after a click on the logo
+$("#logo").click(() => {
+    //TODO:
+    console.log("Dddddddd")
+});
+
+var cookie = {
+    firstPartyDomain: "oxygenrain.com",
+    name: "your-time-login",
+    url: "oxygenrain.com/yourtime/"
+}
+
+browser.cookies.get(cookie)
+    .then((content) => {
+        if (content !== null) {
+            $("#loginStatus").text("Logged in");
+            $("#login")
+                .text("Log out")
+                .attr("href", "javascript:logOut()");
+        }
+    });
+
+function logOut() {
+    browser.cookies.remove(cookie);
+    $("#loginStatus").text("Currently not logged in");
+    $("#login")
+        .text("Log in with Google")
+        .attr("href", "https://oxygenrain.com/yourtime/auth/google.html");
+}
