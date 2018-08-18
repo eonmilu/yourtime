fa2a9ca402b7f1af246f5d86c8f0871cc61b2e20
eonmilu <eonmilu@gmail.com>
2018-08-19 01:42:22 +0200

    Refactor

--- src/firefox/scripts/content.ts
+++ src/firefox/scripts/content.ts
@@ -1,205 +1,206 @@
 const SELECT_URL = "https://oxygenrain.com/yourtime/search";
 const INSERT_URL = "https://oxygenrain.com/yourtime/insert";
-const META = JSON.parse(document.getElementsByTagName('meta')['your-time-meta'].getAttribute('content'));
+const META = JSON.parse($("meta[name='your-time-meta'").attr('content'));
 const DEFAULT_TIMEOUT = 1500;
+const EXTENSION_URL = "https://addons.mozilla.org/firefox/addon/yourtime/";
+const STATUS_CODE = {
+	FOUND: "200",
+	NOT_FOUND: "210",
+	ERROR: "220"
+};
 
-var id: string;
-var lastId: string;
-// Append loader
-var loader = $("<img/>", {
-    src: META.loader,
-    id: "your-time-loader",
-    style: "display: block; margin: auto; margin-top: 5px;"
+var loaderIcon = $("<img/>", {
+	src: META.loaderIconURL,
+	id: "your-time-loader",
+	style: "display: block; margin: auto; margin-top: 5px;"
 }).height("25px");
-loader.appendTo($("#info-contents"));
+loaderIcon.appendTo($("#info-contents"));
 
-// Get ID of YouTube video from URL
-id = window.location.href.match(/v=([^&]*)/)[1];
+var lastId = "";
+var videoID = getCurrentVideoID();
 
-// Player MUST be assigned with document.getElementById, otherwise the YouTube API will not work
+// Must use document.getElementById, otherwise the API will not work
 var player: any = document.getElementById("movie_player");
-player.addEventListener("onStateChange", (statusInteger: any) => {
-    // https://developers.google.com/youtube/iframe_api_reference#Events
-    // Normally we would use -1 or 5 but YouTube's API is unreliable
-    // Therefore we will use 1 (playing) and check if the data has already been loaded
-    console.log(statusInteger);
-    id = window.location.href.match(/v=([^&]*)/)[1];
-    if (statusInteger == 1 && lastId != id) {
-        lastId = id;
-        loader.appendTo($("#info-contents"));
-        onLayoutLoaded();
-    }
+player.addEventListener("onStateChange", (statusInteger: Number) => {
+	// https://developers.google.com/youtube/iframe_api_reference#Events
+	// The UNSTARTED or CUED status should be used.
+	// However, YouTube's IFrame Player API is unreliable,
+	// so we shall use PLAYING and check if the timemarks have been loaded
+	videoID = getCurrentVideoID();
+	if (statusInteger == YT.PlayerState.PLAYING && lastId != videoID) {
+		lastId = videoID;
+		loaderIcon.appendTo($("#info-contents"));
+		onLayoutLoaded();
+	}
 });
-// HACK: Make sure the pause/play event is fired
-player.pauseVideo();
-player.playVideo();
+
+ensureStateChange();
+
+function getCurrentVideoID(): string {
+	let id = window.location.href.match(/v=([^&]*)/)[1];
+	return id;
+}
 
 function onLayoutLoaded() {
-    removeMainStructure();
-    $.ajax({
-        method: "GET",
-        url: SELECT_URL,
-        dataType: "text",
-        data: { v: id },
-        timeout: DEFAULT_TIMEOUT,
-    }).done(rp => {
-        addMainStructure();
+	removeMainStructure();
+	$.ajax({
+		method: "GET",
+		url: SELECT_URL,
+		dataType: "text",
+		data: { v: videoID },
+		timeout: DEFAULT_TIMEOUT,
+	}).always(() => {
+		addMainStructure();
+		$("#your-time-loader").remove();
+	}).done(rawResponse => {
+		// Response's first 3 characters are the status code
+		// Anything else is considered JSON
+		let statusCode = rawResponse.substr(0, 3);
+		let response = rawResponse.substr(3);
 
-        $("#your-time-loader").remove();
-        // Response's first 3 characters will be the status code
-        let statusCode = rp.substr(0, 3);
-        // Anything else is considered JSON
-        let response = rp.substr(3);
+		processResponse(statusCode, response);
+	}).fail((jqXHR, textStatus, error) => {
+		console.log(jqXHR, textStatus, error);
+		addError("220");
+	});
+}
 
-        processResponse(response, statusCode);
-    }).fail((jqXHR, textStatus, error) => {
-        addMainStructure();
-
-        $("#your-time-loader").remove();
-
-        console.log(error);
-        console.log(jqXHR);
-        console.log(textStatus);
-        addError("220");
-    });
+function ensureStateChange() {
+	// HACK: Make sure the pause/play event is fired
+	player.pauseVideo();
+	player.playVideo();
 }
 
 // Transform seconds to ((days):(hours):)minutes:seconds
-function secondsToString(ss: any): string {
-    // Ignore negative seconds and types other than number
-    if (ss < 0 || typeof ss != "number")
-    return undefined;
+function secondsToDate(ss: any): string {
+	// Ignore negative seconds and types other than number
+	if (ss < 0 || typeof ss != "number")
+		return undefined;
 
-    let dd: any = Math.floor(ss / (3600 * 24));
-    ss -= dd * 3600 * 24;
-    let hh: any = Math.floor(ss / 3600);
-    ss -= hh * 3600;
-    let mm: any = Math.floor(ss / 60);
-    ss -= mm * 60;
+	let dd: any = Math.floor(ss / (3600 * 24));
+	ss -= dd * 3600 * 24;
+	let hh: any = Math.floor(ss / 3600);
+	ss -= hh * 3600;
+	let mm: any = Math.floor(ss / 60);
+	ss -= mm * 60;
 
-    // Ensure two-digits
-    [dd, hh, mm, ss] = [dd, hh, mm, ss].map(x => x < 10 ? x = "0" + x : x);
+	// Ensure two-digits
+	[dd, hh, mm, ss] = [dd, hh, mm, ss].map(p => p < 10 ? p = "0" + p : p);
 
-    let time = `${dd}:${hh}:${mm}:${ss}`
+	let time = `${dd}:${hh}:${mm}:${ss}`;
 
-    // Remove unnecessary "00:"s, keeping the last two for formatting
-    return time.replace(/^(00\:){1,2}/gm, "");
+	// Remove unnecessary "00:"s, keeping the last two for formatting
+	return time.replace(/^(00\:){1,2}/gm, "");
 }
 
 // Make numbers human-readable
 function readablizeNumber(n: number): string {
-    let s = ['', 'k', 'M', 'B'];
-    let e = Math.floor(Math.log(n) / Math.log(1000));
-    return Math.round((n / Math.pow(1000, e))) + s[e];
+	let s = ['', 'k', 'M', 'B'];
+	let e = Math.floor(Math.log(n) / Math.log(1000));
+	return Math.round((n / Math.pow(1000, e))) + s[e];
 }
 
-// Add main skeleton, where the children will be added
 function addMainStructure(): void {
-    $("<div/>", { id: "your-time" }).appendTo("#info-contents"); // Main div
+	$("<div/>", {
+		id: "your-time"
+	}).appendTo("#info-contents");
 }
 
 function appendChildToMainStructure(childData: any): void {
-    // TODO: redesing, rewrite with jQuery
-    let submission = document.createElement("div");
-    let votes = document.createElement("div");
-    let upvote = document.createElement("svg");
-    let number = document.createElement("span");
-    let downvote = document.createElement("svg");
-    let seconds = document.createElement("a");
-    let comment = document.createElement("span");
-    let triangle = document.createElement("polygon");
+	// TODO: redesing, rewrite with jQuery
+	let submission = document.createElement("div");
+	let votes = document.createElement("div");
+	let upvote = document.createElement("svg");
+	let number = document.createElement("span");
+	let downvote = document.createElement("svg");
+	let seconds = document.createElement("a");
+	let comment = document.createElement("span");
+	let triangle = document.createElement("polygon");
 
-    submission.className = "submission";
-    votes.className = "votes";
-    upvote.className = "upvote";
-    number.className = "number";
-    downvote.className = "downvote";
-    seconds.className = "seconds";
-    comment.className = "comment";
+	submission.className = "submission";
+	votes.className = "votes";
+	upvote.className = "upvote";
+	number.className = "number";
+	downvote.className = "downvote";
+	seconds.className = "seconds";
+	comment.className = "comment";
 
-    upvote.appendChild(triangle);
-    downvote.appendChild(triangle);
+	upvote.appendChild(triangle);
+	downvote.appendChild(triangle);
 
-    number.innerText = readablizeNumber(childData["votes"]);
-    seconds.innerText = secondsToString(parseInt(childData["time"]));
-    seconds.addEventListener('click', function () {
-        // HACK: bypass TS' type check
-        player["seekTo"](childData["time"]);
-    });
-    seconds.rel = "nofollow";
-    comment.innerText = childData["comment"];
+	number.innerText = readablizeNumber(childData["votes"]);
+	seconds.innerText = secondsToDate(parseInt(childData["time"]));
+	seconds.addEventListener('click', function () {
+		// HACK: bypass TS' type check
+		player["seekTo"](childData["time"]);
+	});
+	seconds.rel = "nofollow";
+	comment.innerText = childData["comment"];
 
-    votes.appendChild(upvote);
-    votes.appendChild(number);
-    votes.appendChild(downvote);
+	votes.appendChild(upvote);
+	votes.appendChild(number);
+	votes.appendChild(downvote);
 
-    submission.appendChild(votes);
-    submission.appendChild(seconds);
-    submission.appendChild(comment);
+	submission.appendChild(votes);
+	submission.appendChild(seconds);
+	submission.appendChild(comment);
 
-    $("#your-time").append(submission);
+	$("#your-time").append(submission);
 }
 
 // Parse and add the response to the DOM
-function processResponse(rp: string, statusCode: string): void {
-    /* STATUS CODES
-    200: Found
-    210: Not found
-    220: Internal error
-    */
-
-    if (statusCode == "200") {
-        let response = JSON.parse(rp);
-        response.forEach(element => {
-            appendChildToMainStructure(element);
-        });
-    } else {
-        addError(statusCode);
-    }
+function processResponse(statusCode: string, rawResponse: string): void {
+	if (statusCode == STATUS_CODE.FOUND) {
+		let response = JSON.parse(rawResponse);
+		response.forEach(appendChildToMainStructure);
+	} else {
+		addError(statusCode);
+	}
 }
 
 function addError(statusCode: string) {
-    let mainTextMsg, secTextMsg, secTextOnclick;
-    switch (statusCode) {
-        case "210":
-            mainTextMsg = "Your Time didn't find any timemarks for this video. "
-            secTextMsg = "Submit your own. "
-            secTextOnclick = addTimemark;
-            break;
-        case "220":
-            mainTextMsg = "Internal server error. "
-            secTextMsg = "Try again later."
-            secTextOnclick = null;
-            break;
-        case "":
-        default:
-            mainTextMsg = "Unknown status code"
-            secTextMsg = "Are you using the latest Your Time version?"
-            secTextOnclick = () => {
-                let win = window.open('https://addons.mozilla.org/en-US/firefox/addon/your-time/', '_blank');
-                win.focus();
-            };
-            break;
-    }
+	let mainTextMsg, secondaryTextMsg, secondaryTextOnclick;
+	switch (statusCode) {
+		case STATUS_CODE.NOT_FOUND:
+			mainTextMsg = "Your Time didn't find any timemarks for this video."
+			secondaryTextMsg = "Submit your own."
+			secondaryTextOnclick = createTimemark;
+			break;
+		case STATUS_CODE.ERROR:
+			mainTextMsg = "Your Time could not connect to the server"
+			secondaryTextMsg = "Try again later."
+			secondaryTextOnclick = null;
+			break;
+		default:
+			mainTextMsg = "Unknown status code"
+			secondaryTextMsg = "Are you using the latest Your Time version?"
+			secondaryTextOnclick = function () {
+				let win = window.open(EXTENSION_URL, "_blank");
+				win.focus();
+			};
+			break;
+	}
 
-    $("<div/>", { id: "your-time-error" }).append(
-        $("<span/>", {
-            class: "main-text"
-        }).text(mainTextMsg),
-        $("<a/>", {
-            class: "secondary-text",
-        }).text(secTextMsg)
-            .click(secTextOnclick)
-    ).appendTo($("#your-time"));
-    $("#your-time").show()
+	$("<div/>", {
+		id: "your-time-error"
+	}).append(
+		$("<span/>", {
+			class: "main-text"
+		}).text(mainTextMsg),
+		$("<a/>", {
+			class: "secondary-text",
+		}).text(secondaryTextMsg)
+			.click(secondaryTextOnclick)
+	).appendTo($("#your-time"));
+	$("#your-time").show()
+}
+
+function createTimemark() {
+
 }
 
 function removeMainStructure(): void {
-    while ($("your-time").length) {
-        $("#your-time").remove();
-    }
-}
-
-function addTimemark() {
-    alert("TODO:");
+	while ($("#your-time").length) {
+		$("#your-time").remove();
+	}
 }
