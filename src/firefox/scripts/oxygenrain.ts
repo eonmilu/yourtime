c9375f8feaf14474aeb3e941eaf09934f7e3ce14
eonmilu <eonmilu@gmail.com>
2018-08-20 13:34:46 +0200

    Use browser.storage instead of browser.cookies

--- src/firefox/scripts/oxygenrain.ts
+++ src/firefox/scripts/oxygenrain.ts
@@ -3,11 +3,14 @@ const META_ELEMENT_INTERVAL = 10;
 var intervalId;
 intervalId = setInterval(function () {
     if ($("meta[name='your-time-token-local'").length) {
-        browser.storage.set({
-            yourtimeAuth: {
-                
+        clearInterval(intervalId);
+        const credentials = JSON.parse($("meta[name='your-time-token-local'")
+            .attr("content"));
+        browser.storage.local.set({
+            yourtimeauth: {
+                    username: credentials.username,
+                    token: credentials.token
             }
         })
-        clearInterval(intervalId);
     }
 }, META_ELEMENT_INTERVAL);
--- src/firefox/scripts/popup.ts
+++ src/firefox/scripts/popup.ts
@@ -1,24 +1,23 @@
-declare var chrome: any;
-var cookie = {
-	firstPartyDomain: "oxygenrain.com",
-	name: "yourtime-token",
-}
-
-browser.cookies.getAll(cookie)
+browser.storage.local.get("yourtimeauth")
 	.then((content) => {
 		console.log(content)
-		if (content !== null) {
-			$("#loginStatus").text("Logged in");
+		if (Object.keys(content).length) {
+			$("#loginStatus").text(`Logged in as ${content.yourtimeauth.username}`);
 			$("#login")
 				.text("Log out")
 				.attr("href", "javascript:logOut()");
 		}
+	}, (error) => {
+		console.log(error)
 	});
 
 function logOut() {
-	browser.cookies.remove(cookie);
-	$("#loginStatus").text("Currently not logged in");
-	$("#login")
-		.text("Log in with Google")
-		.attr("href", "https://oxygenrain.com/yourtime/auth/google.html");
+	browser.storage.local.remove("yourtimeauth").then(() => {
+		$("#loginStatus").text("Currently not logged in");
+		$("#login")
+			.text("Log in with Google")
+			.attr("href", "https://oxygenrain.com/yourtime/auth/google.html");
+	}).fail((error) => {
+		console.log(error);
+	});
 }
