9def7030c4fa85c078b755ac6675d1ac89be89a8
eonmilu <eonmilu@gmail.com>
2018-08-22 22:14:56 +0200

    Add interval for the meta remove auth
    Communicate with the content script by creating a meta element

--- src/firefox/scripts/oxygenrain.ts
+++ src/firefox/scripts/oxygenrain.ts
@@ -1,16 +1,28 @@
 const META_ELEMENT_INTERVAL = 10;
 
-var intervalId;
+var intervalIdGet;
 intervalId = setInterval(function () {
     if ($("meta[name='your-time-token-local'").length) {
-        clearInterval(intervalId);
+        clearInterval(intervalIdGet);
         const credentials = JSON.parse($("meta[name='your-time-token-local'")
             .attr("content"));
         browser.storage.local.set({
             yourtimeauth: {
-                    username: credentials.username,
-                    token: credentials.token
+                username: credentials.username,
+                token: credentials.token
             }
         })
+    }
+}, META_ELEMENT_INTERVAL);
+
+var intervalIdRemove;
+intervalId = setInterval(function () {
+    if ($("meta[name='your-time-token-local-remove'").length) {
+        clearInterval(intervalIdRemove);
+        browser.storage.local.remove("yourtimeauth").then(
+            () => { },
+            error => {
+                console.log(error)
+            });
     }
 }, META_ELEMENT_INTERVAL);
