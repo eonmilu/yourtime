fc01ad803b0643651e180bebf28e20e501a7f828
eonmilu <eonmilu@gmail.com>
2018-08-23 16:50:15 +0200

    Remove meta elements after reading them

--- src/firefox/scripts/oxygenrain.ts
+++ src/firefox/scripts/oxygenrain.ts
@@ -3,7 +3,6 @@ const META_ELEMENT_INTERVAL = 10;
 var intervalIdGet;
 intervalId = setInterval(function () {
     if ($("meta[name='your-time-token-local'").length) {
-        clearInterval(intervalIdGet);
         const credentials = JSON.parse($("meta[name='your-time-token-local'")
             .attr("content"));
         browser.storage.local.set({
@@ -11,18 +10,24 @@ intervalId = setInterval(function () {
                 username: credentials.username,
                 token: credentials.token
             }
-        })
+        });
+
+        clearInterval(intervalIdGet);
+        $("meta[name='your-time-token-local'").remove();
     }
 }, META_ELEMENT_INTERVAL);
 
 var intervalIdRemove;
 intervalId = setInterval(function () {
     if ($("meta[name='your-time-token-local-remove'").length) {
-        clearInterval(intervalIdRemove);
         browser.storage.local.remove("yourtimeauth").then(
-            () => { },
+            () => {
+                console.log("Storage removed")
+            },
             error => {
                 console.log(error)
             });
+        clearInterval(intervalIdRemove);
+        $("meta[name='your-time-token-local-remove'").remove();
     }
 }, META_ELEMENT_INTERVAL);
