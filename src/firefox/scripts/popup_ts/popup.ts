c42daae5d9492389e1ad8e78f88d80ecbe82cc32
eonmilu <eonmilu@gmail.com>
2019-01-20 18:35:08 +0100

    Replace oxygenrain.com with xmi.lu

--- src/firefox/scripts/popup_ts/popup.ts
+++ src/firefox/scripts/popup_ts/popup.ts
@@ -1,5 +1,5 @@
 declare var browser: any;
-const LogInURL = "https://oxygenrain.com/yourtime/auth/auth.html";
+const LogInURL = "https://xmi.lu/yourtime/auth/auth.html";
 
 browser.storage.local.get("yourtimeauth")
 	.then((content) => {
